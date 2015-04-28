69adb3f8af361ffe5d517ded3eb0b0b24a386cf8
Oleg Novikov <n0v0leg@ya.ru>
2015-04-28 19:13:04 +0300

    refactoring

--- app.jl
+++ app.jl
@@ -30,24 +30,20 @@ function isAntisymm(br)
   return true
 end
 
-# maybe immutable/mutable type should be created for binary relations?
-const n = 4
-const reflexivity  = 0x01
-const antisymmetry = 0x02
-const transitivity = 0x04
-br = trues(n,n)
-powers = zeros(8) # how to preset it's type?
-# iteration through all binary relations of given rank n:
-for binary_relation_index = 0:2^n^2-1
-  binary_relation = CreateBinaryRelationFromItsIndex(binary_relation_index)
-  # at this poit binary relation is represented as a boolean matrix
-  properties = 0x00 # clear flags
-  isReflexive (binary_relation) && (properties |= reflexivity)
-  isAntisymm  (binary_relation) && (properties |= antisymmetry)
-  isTransitive(binary_relation) && (properties |= transitivity)
-  powers[properties+1] += 1 # increase the power of the class with given properties
-end
-for property = 1:8
-  print(powers[property],", ")
-end
-print("\n")
+  const n = 4
+  const reflexivity  = 0x01
+  const antisymmetry = 0x02
+  const transitivity = 0x04
+  br = falses(n,n)
+  powers = zeros(Uint64, 8)
+  # iteration through all binary relations of given rank n:
+  for binary_relation_index = 0:2^n^2-1
+    binary_relation = CreateBinaryRelationFromItsIndex(binary_relation_index)
+    # at this poit binary relation is represented as a boolean matrix
+    properties = 0x00 # clear flags
+    isReflexive (binary_relation) && (properties |= reflexivity)
+    isAntisymm  (binary_relation) && (properties |= antisymmetry)
+    isTransitive(binary_relation) && (properties |= transitivity)
+    powers[properties+1] += 1 # increase the power of the class with given properties
+  end
+  print(powers,"\n")
