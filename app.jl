97fd7765b887b11cb954e3ac6f38c1a0c8c6fbba
Oleg Novikov <n0v0leg@ya.ru>
2015-04-28 18:17:58 +0300

    refactoring&bugfixes

--- app.jl
+++ app.jl
@@ -1,6 +1,6 @@
 function CreateBinaryRelationFromItsIndex(binary_relation_index)
-  for i = 1:n^2
-    br[i] = convert(Bool, binary_relation_index & 1)
+  for i = 1:n, j=1:n
+    br[i,j] = convert(Bool, binary_relation_index & 1)
     binary_relation_index >>>= 1
   end
   return br
@@ -20,30 +20,34 @@ function isTransitive(br)
       br[i,k] && br[k,j] && return false
     end
   end
-return true
+  return true
 end
 
 function isAntisymm(br)
-  for i = 1:n, j = 2:i-1
+  for i = 2:n, j = 1:i-1
     br[i,j] && br[j,i] && return false
   end
   return true
 end
 
 # maybe immutable/mutable type should be created for binary relations?
-const n = 3
+const n = 4
 const reflexivity  = 0x01
 const antisymmetry = 0x02
 const transitivity = 0x04
 br = trues(n,n)
 powers = zeros(8) # how to preset it's type?
 # iteration through all binary relations of given rank n:
-for binary_relation_index = 0:2^n^2
+for binary_relation_index = 0:2^n^2-1
   binary_relation = CreateBinaryRelationFromItsIndex(binary_relation_index)
   # at this poit binary relation is represented as a boolean matrix
   properties = 0x00 # clear flags
-  isReflexive (binary_relation) && properties |= reflexivity
-  isAntisymm  (binary_relation) && properties |= antisymmetry
-  isTransitive(binary_relation) && properties |= transitivity
+  isReflexive (binary_relation) && (properties |= reflexivity)
+  isAntisymm  (binary_relation) && (properties |= antisymmetry)
+  isTransitive(binary_relation) && (properties |= transitivity)
   powers[properties+1] += 1 # increase the power of the class with given properties
 end
+for property = 1:8
+  print(powers[property],", ")
+end
+print("\n")
